9d82c9dda07bde12687d7c92a16c349b6e24f02e
Samary Cardozo <samarycardozo@gmail.com>
2025-11-04 11:59:22 -0500

    Añadir punto 1.

---
+++ 1.Vehiculos.js
@@ -0,0 +1,7 @@
+// Ejercicio 1: Sistema básico de vehículos
+// Crea un sistema de clases para representar diferentes tipos de vehículos. Debes crear:
+// 1.Una clase Vehiculo con propiedades: marca, modelo, año y un 
+// método obtenerDetalles() que devuelva un string con toda la información.
+// 2.Una clase Coche que herede de Vehiculo y añada la propiedad numeroPuertas.
+// 3.Una clase Moto que herede de Vehiculo y añada la propiedad cilindrada.
+// Crea instancias de ambas clases hijas y muestra sus detalle
